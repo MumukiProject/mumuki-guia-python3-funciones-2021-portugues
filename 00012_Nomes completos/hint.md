4db079f316c663e4e9e06a61572cfcff8678adc8
rocio@mumuki.org <rocio@mumuki.org>
2022-08-19 20:05:05 +0000

    Mumuki Export on 2022-08-19 20:05:05 +0000

---
+++ 00012_Nomes completos/hint.md
@@ -0,0 +1,18 @@
+Lembre que podemos verificar o tamanho das strings por meio de `len`...
+
+```python
+ム len("biblioteca")
+10
+ム len("babel")
+5
+```
+
+...e também podemos _concatená-las_, ou seja, obter **uma nova** juntando duas strings:
+
+```python
+ム "aa" + "bb"
+"aabb"
+ム "suas prateleiras " + "registram todas as combinações"
+"suas prateleiras registram todas as combinações"
+```
+
--- 00012_Nomes completos/test.py
+++ 00012_Nomes completos/test.py
@@ -1,19 +1,19 @@
 
   def test_longitud_nombre_completo_Cosme_Fulanito(self):
-    self.assertEqual(longitud_nombre_completo("Cosme", "Fulanito"), 14)
+    self.assertEqual(longitude_nome_completo("Cosme", "Fulanito"), 14)
 
   def test_longitud_nombre_completo_John_Snow(self):
-    self.assertEqual(longitud_nombre_completo("John", "Snow"), 9)
+    self.assertEqual(longitude_nome_completo("John", "Snow"), 9)
 
   def test_longitud_nombre_completo_Juana_Azurduy(self):
-    self.assertEqual(longitud_nombre_completo("Juana", "Azurduy"), 13)
+    self.assertEqual(longitude_nome_completo("Juana", "Azurduy"), 13)
     
   def test_no_falta_contemplar_el_espacio(self):
     resultado = False
     try:
       resultado = [
-        longitud_nombre_completo("", ""),
-        longitud_nombre_completo("abc", "d"),
+        longitude_nome_completo("", ""),
+        longitude_nome_completo("abc", "d"),
       ] == [0, 4]
     except:
       pass
