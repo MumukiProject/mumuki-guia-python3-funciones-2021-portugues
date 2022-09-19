3c32b87ed72c31e70a36da58eeabb5d44a1987ec
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 20:00:04 +0000

    Mumuki Export on 2022-09-19 20:00:04 +0000

--- 00011_Terminando a semana/test.py
+++ 00011_Terminando a semana/test.py
@@ -1,14 +1,14 @@
 
-  def test_el_sabado_es_fin_de_semana(self):
+  def test_o_sabado_e_fim_de_semana(self):
     self.assertTrue(e_fim_de_semana("sábado") or e_fim_de_semana("sabado"))
 
-  def test_el_domingo_es_fin_de_semana(self):
+  def test_o_domingo_e_fim_de_semana(self):
     self.assertTrue(e_fim_de_semana("domingo"))
 
-  def test_el_lunes_no_es_fin_de_semana(self):
+  def test_a_segunda_nao_e_fim_de_semana(self):
     self.assertFalse(e_fim_de_semana("lunes"))
 
-  def test_el_viernes_no_es_fin_de_semana(self):
+  def test_a_sexta_nao_e_fim_de_semana(self):
     self.assertFalse(e_fim_de_semana("viernes"))
 
 
