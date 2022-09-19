ddf442286c045956c0451e3279b5dd40f5532b26
rocio@mumuki.org <rocio@mumuki.org>
2022-09-19 19:40:57 +0000

    Mumuki Export on 2022-09-19 19:40:57 +0000

--- 00005_Plano Combo 2_0/test.py
+++ 00005_Plano Combo 2_0/test.py
@@ -1,23 +1,23 @@
 
-  def test_anterior1_es_0(self):
+  def test_anterior_1_e_0(self):
     self.assertEqual(anterior(1), 0);
 
-  def test_anterior_10_es_9(self):
+  def test_anterior_10_e_9(self):
     self.assertEqual(anterior(10), 9)
 
-  def test_triple_1_es_3(self):
+  def test_triplo_1_e_3(self):
     self.assertEqual(triplo(1), 3)
 
-  def test_triple_3_es_9(self):
+  def test_triplo_3_e_9(self):
     self.assertEqual(triplo(3), 9)
 
-  def test_anterior_del_triple_1_es_2(self):
+  def test_anterior_do_triplo_1_e_2(self):
     self.assertEqual(anterior_do_triplo(1), 2)
 
-  def test_anterior_del_triple_3_es_8(self):
+  def test_anterior_do_triplo_3_e_8(self):
     self.assertEqual(anterior_do_triplo(3), 8)
 
-  def test_anterior_del_triple_10_es_29(self):
+  def test_anterior_do_triplo_10_e_29(self):
     self.assertEqual(anterior_do_triplo(10), 29)
 
 
