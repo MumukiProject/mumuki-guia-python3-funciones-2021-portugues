3d2d9328a872be18d36aab7ab899b99c30ae6418
rocio@mumuki.org <rocio@mumuki.org>
2022-09-20 17:29:26 +0000

    Mumuki Export on 2022-09-20 17:29:26 +0000

--- 00009_Livros de boa memória/test.py
+++ 00009_Livros de boa memória/test.py
@@ -1,14 +1,14 @@
 
-  def test_gosta_de_ler_25_e_verdadeiro(self):
+  def test_gosta_de_ler_25_é_verdadeiro(self):
     self.assertTrue(gosta_de_ler(25))
   
-  def test_gosta_de_ler_80_e_verdadeiro(self):
+  def test_gosta_de_ler_80_é_verdadeiro(self):
     self.assertTrue(gosta_de_ler(80))
   
-  def test_gosta_de_ler_1_e_falso(self):
+  def test_gosta_de_ler_1_é_falso(self):
     self.assertFalse(gosta_de_ler(1))
   
-  def test_gosta_de_ler_15_e_falso(self):
+  def test_gosta_de_ler_15_é_falso(self):
     self.assertFalse(gosta_de_ler(15))
   
   
