dc810cf7c095b9c4f524103eacff57d1166c9d6c
rocio@mumuki.org <rocio@mumuki.org>
2022-09-20 17:27:26 +0000

    Mumuki Export on 2022-09-20 17:27:26 +0000

--- 00004_A hora da verdade/test.py
+++ 00004_A hora da verdade/test.py
@@ -1,12 +1,12 @@
 class Test(unittest.TestCase):
 
-  def test_as_11_nao_sao_a_hora_da_verdade(self):
+  def test_às_11_não_são_a_hora_da_verdade(self):
     self.assertFalse(e_hora_da_verdade(11))
 
-  def test_as_14_nao_sao_a_hora_da_verdade(self):
+  def test_às_14_não_são_a_hora_da_verdade(self):
     self.assertFalse(e_hora_da_verdade(14))
     
-  def test_as_12_sao_a_hora_da_verdade(self):
+  def test_às_12_são_a_hora_da_verdade(self):
     self.assertTrue(e_hora_da_verdade(12))
 
 
