cab5c0b2631eae48530d7bd0637daa60645d899c
rocio@mumuki.org <rocio@mumuki.org>
2022-09-20 17:24:37 +0000

    Mumuki Export on 2022-09-20 17:24:37 +0000

--- 00018_Funções que não funcionam/test.py
+++ 00018_Funções que não funcionam/test.py
@@ -1,20 +1,20 @@
 
-  def test_metade_de_2_e_1(self):
+  def test_metade_de_2_é_1(self):
     self.assertEqual(metade(2), 1)
 
 
-  def test_metade_de_20_e_10(self):
+  def test_metade_de_20_é_10(self):
     self.assertEqual(metade(20), 10)
 
 
-  def test_metade_de_10_e_5(self):
+  def test_metade_de_10_é_5(self):
     self.assertEqual(metade(10), 5)
     
 
-  def test_soma_comprimentos_de_ola_e_mundo_a_9(self):
+  def test_soma_comprimentos_de_olá_e_mundo_é_9(self):
     self.assertEqual(soma_longitudes("hola", "mundo"), 9)
 
 
-  def test_soma_comprimentos_de_chova_e_cafe_e_6(self):
+  def test_soma_comprimentos_de_chova_e_cafe_é_6(self):
     self.assertEqual(soma_longitudes("llueva", "café"), 10)
 
