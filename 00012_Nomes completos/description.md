5f834bdb263d8b6d04816a859b88e216d723a727
nadia@mumuki.org <nadia@mumuki.org>
2023-01-05 16:17:43 +0000

    Mumuki Export on 2023-01-05 16:17:42 +0000

--- 00012_Nomes completos/description.md
+++ 00012_Nomes completos/description.md
@@ -1,9 +1,9 @@
 Praticando o que vimos!
 
-> Defina uma função `longitude_nome_completo`, que utilize um nome e um sobrenome, e retorne a longitude total, **contando um espaço extra** para separar ambos:
+> Defina uma função `comprimento_nome_completo`, que utilize um nome e um sobrenome, e retorne a longitude total, **contando um espaço extra** para separar ambos:
 >
 >```python
-> ム longitude_nome_completo("Cosme", "Fulanito")
+> ム comprimento_nome_completo("Cosme", "Fulanito")
 >14
 >```
 
--- 00012_Nomes completos/test.py
+++ 00012_Nomes completos/test.py
@@ -1,19 +1,19 @@
 
   def test_longitude_nome_completo_Cosme_Fulanito(self):
-    self.assertEqual(longitude_nome_completo("Cosme", "Fulanito"), 14)
+    self.assertEqual(comprimento_nome_completo("Cosme", "Fulanito"), 14)
 
   def test_longitude_nome_completo_John_Snow(self):
-    self.assertEqual(longitude_nome_completo("John", "Snow"), 9)
+    self.assertEqual(comprimento_nome_completo("John", "Snow"), 9)
 
   def test_longitude_nome_completo_Juana_Azurduy(self):
-    self.assertEqual(longitude_nome_completo("Juana", "Azurduy"), 13)
+    self.assertEqual(comprimento_nome_completo("Juana", "Azurduy"), 13)
     
   def test_não_falta_contemplar_o_espaço(self):
     resultado = False
     try:
       resultado = [
-        longitude_nome_completo("", ""),
-        longitude_nome_completo("abc", "d"),
+        comprimento_nome_completo("", ""),
+        comprimento_nome_completo("abc", "d"),
       ] == [0, 4]
     except:
       pass
